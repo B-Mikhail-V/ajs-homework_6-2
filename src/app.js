ed24daa67202f15053388990f9593b9ab86bee7b
Mikail Bakulin <bakulin.mv@gmail.com>
2022-12-14 13:34:22 +0300

    Corr_Task

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-export default function getAttack(obj) {
+export default function getAttack({ special }) {
   const attackArray = [];
   /* eslint-disable-next-line */
-  for (const value of Object.values(obj.special)) {
+  for (const att of special) {
     const {
       id, name, icon, description = 'Описание недоступно',
-    } = value;
+    } = att;
     const arrayTemp = [id, name, icon, description];
     attackArray.push(arrayTemp);
   }
-
   return attackArray;
 }
